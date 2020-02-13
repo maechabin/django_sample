c5f17caebea7dbfe816c877e43182669d704dfe3
maeda <takanori@stroly.jp>
2020-02-13 20:00:50 +0900

    チュートリアルその3: get_object_or_404()

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
@@ -13,9 +13,6 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        # pk は primary_key のこと
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    # pk は primary_key のこと
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
